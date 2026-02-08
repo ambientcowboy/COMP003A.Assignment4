b0bae36e9ca3acaf4cde0b9e190062b240bfb14f
ambientcowboy <holysignals@protonmail.com>
2026-02-07 21:42:11 -0800

    Adde lines 7-33. Initiated do-while loop for routine, added int variables to count and compare routine steps.

--- Program.cs
+++ Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        //This 
+        int maxSteps = 6;
+        int restrictedStep = 3;
+        int safetyStopsAtStep = 5;
+        int nextStep = 1;
+        int stepsDone = 0;
+        bool safetyStop = false;
+        bool keepRunning = true;
+        do
+        {
+            Console.WriteLine();
+            Console.WriteLine("1. Start/Continue Routine");
+            Console.WriteLine("2. View Progress");
+            Console.WriteLine("3. Exit");
+            Console.WriteLine();
+            Console.Write("Enter Choice: ");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    if (safetyStopped)
+                    {
+                        Console.WriteLine("Safety limit reached. Routine stopped.");
+                        break;
+                    }
+            }
+        } while (keepRunning);
     }
 }
